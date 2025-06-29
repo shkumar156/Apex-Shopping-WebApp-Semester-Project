f1209b3c93239766c2df04fc2e3acb0fbf9d2fac
Muhammad Umar <muhammadumar8741@gmail.com>
2025-06-29 05:51:49 +0500

    Making My Web App Responsive

--- Frontend/src/components/AdminSideBar.js
+++ Frontend/src/components/AdminSideBar.js
@@ -7,29 +7,35 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTachometerAlt, faList, faEdit, faTrash, faPlus, faCog, faMap, faUser } from '@fortawesome/free-solid-svg-icons';
 
 function AdminSideBar() {
+  const closeSidebar = () => {
+    const sidebar = document.querySelector('.admin-side-bar');
+    if (sidebar && sidebar.classList.contains('show')) {
+      sidebar.classList.remove('show');
+    }
+  };
   return (
     <>
       <div className="list-group admin-side-bar">
-      <Link to="/map" className="list-group-item side-bar-list list-group-item-action !active">
-          <FontAwesomeIcon icon={faMap} /> Maps
+        <Link to="/map" className="list-group-item side-bar-list list-group-item-action !active" onClick={closeSidebar}>
+          <FontAwesomeIcon icon={faMap} /> <span className="sidebar-text">Maps</span>
         </Link>
-        <Link to="/dashboard" className="list-group-item side-bar-list list-group-item-action !active">
-          <FontAwesomeIcon icon={faUser} /> Calender
+        <Link to="/dashboard" className="list-group-item side-bar-list list-group-item-action !active" onClick={closeSidebar}>
+          <FontAwesomeIcon icon={faUser} /> <span className="sidebar-text">Calender</span>
         </Link>
-        <Link to="/add" className="list-group-item side-bar-list list-group-item-action !active">
-          <FontAwesomeIcon icon={faList} /> All Products
+        <Link to="/add" className="list-group-item side-bar-list list-group-item-action !active" onClick={closeSidebar}>
+          <FontAwesomeIcon icon={faList} /> <span className="sidebar-text">All Products</span>
         </Link>
-        <Link to="/confirm" className="list-group-item side-bar-list list-group-item-action !active">
-          <FontAwesomeIcon icon={faEdit} /> Update Product
+        <Link to="/confirm" className="list-group-item side-bar-list list-group-item-action !active" onClick={closeSidebar}>
+          <FontAwesomeIcon icon={faEdit} /> <span className="sidebar-text">Update Product</span>
         </Link>
-        <Link to="/del" className="list-group-item side-bar-list list-group-item-action !active">
-          <FontAwesomeIcon icon={faTrash} /> Delete Product
+        <Link to="/del" className="list-group-item side-bar-list list-group-item-action !active" onClick={closeSidebar}>
+          <FontAwesomeIcon icon={faTrash} /> <span className="sidebar-text">Delete Product</span>
         </Link>
-        <Link to="/dash" className="list-group-item side-bar-list list-group-item-action !active">
-          <FontAwesomeIcon icon={faPlus} /> Create Product
+        <Link to="/dash" className="list-group-item side-bar-list list-group-item-action !active" onClick={closeSidebar}>
+          <FontAwesomeIcon icon={faPlus} /> <span className="sidebar-text">Create Product</span>
         </Link>
-        <Link to="/settings" className="list-group-item side-bar-list list-group-item-action !active">
-          <FontAwesomeIcon icon={faCog} /> Settings
+        <Link to="/settings" className="list-group-item side-bar-list list-group-item-action !active" onClick={closeSidebar}>
+          <FontAwesomeIcon icon={faCog} /> <span className="sidebar-text">Settings</span>
         </Link>
       </div>
     </>
--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -7,12 +7,20 @@ function Navbar({ setUser, setAdminUser, user, admin }) {
     localStorage.clear();
     setAdminUser(false);
     setUser(false);
+    closeNavbar();
+  };
+
+  const closeNavbar = () => {
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    if (navbarCollapse && navbarCollapse.classList.contains('show')) {
+      navbarCollapse.classList.remove('show');
+    }
   };
 
   return (
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/home">
+        <Link className="navbar-brand" to="/home" onClick={closeNavbar}>
           <img className="logo" src={logo} alt="Logo" />
         </Link>
         <button
@@ -32,23 +40,28 @@ function Navbar({ setUser, setAdminUser, user, admin }) {
             {(user || admin) && (
               <>
                 <li className="nav-item">
-                  <Link to="/home" className="nav-link txt !active">Home</Link>
+                  <Link to="/home" className="nav-link txt !active" onClick={closeNavbar}>Home</Link>
                 </li>
                 <li className="nav-item">
-                  <Link to="/aboutus" className="nav-link txt !active">About Us</Link>
+                  <Link to="/aboutus" className="nav-link txt !active" onClick={closeNavbar}>About Us</Link>
                 </li>
                 <li className="nav-item">
-                  <Link to="/menucard" className="nav-link txt !active">Menu</Link>
+                  <Link to="/menucard" className="nav-link txt !active" onClick={closeNavbar}>Menu</Link>
                 </li>
                 <li className="nav-item">
-                  <Link to="/contactus" className="nav-link txt !active">Contact Us</Link>
+                  <Link to="/contactus" className="nav-link txt !active" onClick={closeNavbar}>Contact Us</Link>
                 </li>
                 <li className="nav-item">
-                  <Link to="/sale" className="nav-link txt !active">Sale Items</Link>
+                  <Link to="/sale" className="nav-link txt !active" onClick={closeNavbar}>Sale Items</Link>
                 </li>
                 <li className="nav-item">
-                  <Link to="/track" className="nav-link txt !active">Track Orders</Link>
+                  <Link to="/track" className="nav-link txt !active" onClick={closeNavbar}>Track Orders</Link>
                 </li>
+                {admin && (
+                  <li className="nav-item">
+                    <Link to="/add" className="nav-link txt !active" onClick={closeNavbar}>Admin Dashboard</Link>
+                  </li>
+                )}
               </>
             )}
 
@@ -67,10 +80,10 @@ function Navbar({ setUser, setAdminUser, user, admin }) {
                 </a>
                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                   <li>
-                    <Link to="/signup" className="dropdown-item">Sign Up</Link>
+                    <Link to="/signup" className="dropdown-item" onClick={closeNavbar}>Sign Up</Link>
                   </li>
                   <li>
-                    <Link to="/customerlogin" className="dropdown-item">Login</Link>
+                    <Link to="/customerlogin" className="dropdown-item" onClick={closeNavbar}>Login</Link>
                   </li>
                 </ul>
               </li>
@@ -79,7 +92,7 @@ function Navbar({ setUser, setAdminUser, user, admin }) {
             {/* Admin button - only show when NOT logged in as user or admin */}
             {!(user || admin) && (
               <li className="nav-item">
-                <Link to="/adminlogin" className="nav-link txt !active">Admin</Link>
+                <Link to="/adminlogin" className="nav-link txt !active" onClick={closeNavbar}>Admin</Link>
               </li>
             )}
 
